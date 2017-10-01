a8f65a514b4c20e61e5e8438b1925c0e45e90893
Sourav Bhattacharya <sourav.bhattacharya3@gmail.com>
2017-10-01 22:18:55 +0530

    Add project files.

---
+++ WebAppMvcCoreTest2/Helpers/WebApiClient.cs
@@ -0,0 +1,47 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using WebAppMvcCoreTest2.Models;
+
+namespace WebAppMvcCoreTest2.Helpers
+{
+    public class WebApiClient
+    {
+        public List<PersonModel> ViewAllPerson()
+        {
+            List<PersonModel> personList = null;
+
+            try
+            {
+                HttpWebRequest MyReq = (HttpWebRequest)WebRequest.Create("http://completeaiouserapi.azurewebsites.net/api/UserApi");
+                MyReq.ContentType = "application/json";
+
+                HttpWebResponse MyRes = (HttpWebResponse)MyReq.GetResponseAsync().Result;
+
+                if (MyRes.StatusCode == HttpStatusCode.OK)
+                {
+                    StreamReader sr = new StreamReader(MyRes.GetResponseStream());
+                    string res = sr.ReadToEnd();
+
+                    personList = JsonConvert.DeserializeObject<List<PersonModel>>(res);
+
+                    //foreach (var item in personList)
+                    //{
+                    //    Console.WriteLine(item.UserFirstName);
+                    //}
+                }
+            }
+            catch (System.Exception ex)
+            {
+                //Console.WriteLine(ex.Message);
+                personList = null;
+            }
+
+            return personList;
+        }
+    }
+}
---
+++ WebAppMvcCoreTest2/Models/PersonModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppMvcCoreTest2.Models
+{
+    public class PersonModel
+    {
+        public int UserId { get; set; }
+        public string UserFirstName { get; set; }
+        public string UserEmail { get; set; }
+        public string UserPassword { get; set; }
+    }
+}
